ae6654d58bea1ac92482e02024ea32c15897988c
lopesvictor1 <victorlopesvictor@outlook.com>
2020-10-26 19:23:07 -0300

    client working

--- client_s.py
+++ client_s.py
@@ -17,22 +17,23 @@ clocks = [1, 3, 5]
 
 
 
-def count_time():
+def count_time(value):
     valid = False
     count = 0
     LOCAL_TIME = 0
+    print("Clock: {}".format(value))
     while(True):
         if count > 20:
             valid = False
             count = 0
-        x = random.randint(0, 2)
-        LOCAL_TIME += clocks[x]   
+        LOCAL_TIME += value   
         count += 1
-        print("Local time: %d", LOCAL_TIME)
+        print("Local time: {}".format(LOCAL_TIME))
         time.sleep(1)
 
+value = clocks[random.randint(0, 2)]
 
-threading.Thread(target=count_time,).start()
+threading.Thread(target=count_time, args=(value,)).start()
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))    
     s.sendall(comandos[0].encode("utf-8"))
